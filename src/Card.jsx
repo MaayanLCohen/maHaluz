ef4740dd164e1ffeb2494d217173058e9c7287a9
MaayanLCohen <maayan.lital@gmail.com>
2021-09-30 23:25:46 +0300

    first version

--- src/Card.jsx
+++ src/Card.jsx
@@ -14,18 +14,18 @@ class Card extends Component {
         return (
             <div className="card1">
                 <div className="dargImg">
-                    <img src={bars} width="100%" alt="image" />
+                    <img src={bars} width="100%" alt="icon" />
                 </div>
                 <div className="secondDiv">
                     <div className="time">
-                        {(item.showInput || item.done || item.showSelect) && <img src={trash} className="trashIcon" onClick={() => this.props.removeData(index)} alt="image" />}
+                        {(item.showInput || item.done || item.showSelect) && <img src={trash} className="trashIcon" onClick={() => this.props.removeData(index)} alt="icon" />}
                         <div>{item.time}</div>
                     </div>
                     <div className="data">
                         {(!item.done && !item.showSelect && !item.showInput) ?
                             <div className="choosing">
                                 <Archive setSelect={this.props.setSelect} archive={this.props.archive} content={item.content} itemIndex={index} />
-                                {<img src={edit} className="iconChoose icon" onClick={() => this.props.setShowing(true, index)} alt="image" />}
+                                {<img src={edit} className="iconChoose icon" onClick={() => this.props.setShowing(true, index)} alt="icon" />}
                             </div> :
                             <div className="filling">
                                 {item.done && <div className="content">{item.content}</div>}
@@ -35,7 +35,7 @@ class Card extends Component {
                                         onChange={(e) => this.props.handleChange(e, index)}
                                         onKeyDown={(e) => this.props.handleKeyDown(e, index)}
                                     />}
-                                    {<img src={done} className="vi" onClick={() => this.props.setDone(index)} alt="image" />}
+                                    {<img src={done} className="vi" onClick={() => this.props.setDone(index)} alt="icon" />}
                                 </div>}
                             </div>}
                     </div>
